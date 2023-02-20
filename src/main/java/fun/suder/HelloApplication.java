4aeb99f89fd5f2ade63853df76cb6bc6daca938e
topsuder <suders@vip.qq.com>
2023-02-20 17:52:07 +0800

    build(graalVM-example): 打包机制尝试
    {17:14}~/Desktop/graalVM-example:master ✗ ➭ native-image
    fun.suder.HelloApplication
    
    ========================================================================================================================
    GraalVM
    Native Image: Generating 'fun.suder.helloapplication'
    (executable)...
    ========================================================================================================================
    [1/7]
    Initializing...
    (14.1s @ 0.10GB)
     Version info: 'GraalVM 22.3.1 Java 17 CE'
     Java
    version info: '17.0.6+10-jvmci-22.3-b13'
     C compiler: cc (apple, x86_64,
    14.0.0)
     Garbage collector: Serial GC
    [2/7] Performing analysis...
    [*****]
    (25.0s @ 0.32GB)
       2,802 (73.54%) of  3,810 classes reachable
       3,360
    (50.75%) of  6,621 fields reachable
      12,344 (42.83%) of 28,818 methods
    reachable
          27 classes,     0 fields, and   313 methods registered
    for reflection
          58 classes,    59 fields, and    52 methods
    registered for JNI access
           4 native libraries: -framework
    Foundation, dl, pthread, z
    [3/7] Building universe...
    (2.8s @ 0.77GB)
    [4/7] Parsing methods...      [**]
    (2.1s @ 1.38GB)
    [5/7] Inlining methods...     [***]
    (1.8s @ 0.58GB)
    [6/7] Compiling methods...    [****]
    (18.4s @ 0.75GB)
    [7/7] Creating image...
    (2.1s @ 1.16GB)
       3.96MB (35.78%) for code area:     7,073 compilation
    units
       6.91MB (62.36%) for image heap:   96,833 objects and 5
    resources
     211.04KB ( 1.86%) for other data
      11.08MB in
    total
    ------------------------------------------------------------------------------------------------------------------------
    Top
    10 packages in code area:                               Top 10 object
    types in image heap:
     659.76KB java.util
    907.59KB java.lang.String
     328.28KB java.lang
    877.01KB byte[] for code metadata
     264.69KB java.text
    859.78KB byte[] for general heap data
     216.40KB java.util.regex
    614.91KB java.lang.Class
     194.76KB java.util.concurrent
    540.25KB byte[] for java.lang.String
     146.95KB java.math
    438.38KB java.util.HashMap$Node
     116.74KB java.lang.invoke
    218.91KB com.oracle.svm.core.hub.DynamicHubCompanion
     114.75KB
    com.oracle.svm.core.genscavenge                    212.78KB
    java.util.HashMap$Node[]
     103.72KB java.util.logging
    159.95KB java.lang.String[]
      95.18KB java.util.stream
    154.92KB java.util.concurrent.ConcurrentHashMap$Node
       1.72MB for 115
    more packages                                1.52MB for 766 more object
    types
    ------------------------------------------------------------------------------------------------------------------------
    
    0.7s (0.9% of total time) in 20 GCs | Peak RSS: 2.82GB | CPU load:
    2.64
    ------------------------------------------------------------------------------------------------------------------------
    Produced
    artifacts:
    
    /Users/topsuder/Desktop/graalVM-example/fun.suder.helloapplication
    (executable)
    
    /Users/topsuder/Desktop/graalVM-example/fun.suder.helloapplication.build_artifacts.txt
    (txt)

---
+++ src/main/java/fun/suder/HelloApplication.java
@@ -0,0 +1,20 @@
+package fun.suder;
+
+/**
+ * <span>Form File</span>
+ * <p>Description</p>
+ * <p>Copyright: Copyright (c) 2022 版权</p>
+ * <p>Company:QQ 752340543</p>
+ *
+ * @author topsuder
+ * @version v1.0.0
+ * @DATE 2023/2/20-15:35
+ * @Description
+ * @see fun.suder graalvm-example
+ */
+public class HelloApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello GraalVM!");
+    }
+}
